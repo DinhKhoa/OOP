93360f5940cacaa4a16a89197d70b296a98a565c
Khoa nguyen dinh <khoadeptrai@gmail.com>
2024-09-16 21:26:04 +0700

    Create BookClass

---
+++ OOP/BookClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class BookClass
+    {
+        public string TenSach { get; set; }
+        public string TacGia { get; set; }
+        public int NamXB { get; set; }
+        public int SoLuong { get; set; }
+
+        public BookClass()
+        {
+            SoLuong = 1;
+        }
+        public BookClass(int soLuong)
+        {
+            SoLuong = soLuong;
+        }
+        public BookClass(string tenSach)
+        {
+            TenSach = tenSach;
+        }
+        public BookClass(string tenSach, int soLuong)
+        {
+            TenSach = tenSach;
+            SoLuong = soLuong;
+        }
+
+        public void Read()
+        {
+            TenSach = Console.ReadLine();
+            TacGia = Console.ReadLine();
+            NamXB = int.Parse(Console.ReadLine());
+            SoLuong = int.Parse(Console.ReadLine());
+        }
+        public void WriteDetails()
+        {
+            Console.WriteLine($"\nTen sach: {TenSach}\nTac gia: {TacGia}\nNam xuat ban: {NamXB}\nSo luong: {SoLuong}");
+        }
+
+        ~BookClass()
+        {
+            Console.WriteLine("Du lieu da duoc xoa.");
+        }
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Another commit");
+﻿using System.Data;
+using System.Net.NetworkInformation;
+
+namespace OOP
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BookClass n = new BookClass("DAD", 4);
+            BookClass m = new BookClass("DADAD");
+            BookClass h = new BookClass(13);
+
+            //n.TenSach = Console.ReadLine();
+            //n.TacGia = Console.ReadLine();
+            //n.NamXB = int.Parse(Console.ReadLine());
+            //n.SoLuong = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"\nTen sach: {n.TenSach}\nTac gia: {n.TacGia}\nNam xuat ban: {n.NamXB}\nSo luong: {n.SoLuong}");
+            //Console.WriteLine($"\nTen sach: {m.TenSach}\nTac gia: {m.TacGia}\nNam xuat ban: {m.NamXB}\nSo luong: {m.SoLuong}");
+            //Console.WriteLine($"\nTen sach: {h.TenSach}\nTac gia: {h.TacGia}\nNam xuat ban: {h.NamXB}\nSo luong: {h.SoLuong}");
+
+            //PrintBook(n);
+            //PrintBook(m);
+            //PrintBook(h);
+
+            n.Read();
+
+            n.WriteDetails();
+            m.WriteDetails();
+            h.WriteDetails();
+
+
+            Object dasd = new Object();
+            List<int> List = new List<int>() { };
+            //Implicit Explicit
+        }
+        public static void PrintBook(BookClass book)
+        {
+            Console.WriteLine($"\nTen sach: {book.TenSach}\nTac gia: {book.TacGia}\nNam xuat ban: {book.NamXB}\nSo luong: {book.SoLuong}");
+        }
+    }
+}
